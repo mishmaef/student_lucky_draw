5ac84487adcb96dc739cb5b8dc46781b771d18aa
mishmaef <mishmaefabcd@gmail.com>
2026-07-05 16:54:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,3 @@
+import students
+print("       student lucky draw system        ")
+print(f"The lucky student is: {students.pick_winner()}")    
---
+++ students.py
@@ -0,0 +1,6 @@
+import random
+
+all_students = ["Mahi", "Mish", "Ephi", "Robi", "Mule", "semir", "Nina", "Zara", "Kiki", "Rico"]
+
+def pick_winner():
+    return random.choice(all_students)
